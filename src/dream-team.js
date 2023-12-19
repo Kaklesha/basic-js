fd7fb49bd793d53d55391556d49b6afa3c36e3f0
Kirill Kornilov <k-korni1ov@yandex.ru>
2023-12-20 01:04:41 +0400

    fix: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,23 +1,38 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  let dump='';
+  let subdump='';
+for (var prop in members) {
+
+if(typeof(members[prop])!="string") continue;
+
+subdump= members[prop].replace(/\s/g,'') 
+
+if(subdump.toString().charAt(0).toUpperCase() == subdump.toString().charAt(0).toUpperCase() && typeof(subdump)!="number"){ dump+=subdump.toString().charAt(0);}
+
+}
+let sortString = (dump) => {
+return dump.split("").sort().join("");
+};
+
+console.log( sortString(dump))
+  return sortString(dump).toUpperCase();
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
